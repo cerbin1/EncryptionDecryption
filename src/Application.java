6e25ea1531679943d1d89ff4c4bbfb5751cad4de
cerbin <bleble@d.com>
2016-10-29 18:59:55 +0200

    Added simple coding word, change characters into X.

--- src/Application.java
+++ src/Application.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Application {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String usersText = scanner.nextLine();
+        String newWord = "";
+        for (int i = 0; i < usersText.length(); i++) {
+            newWord += 'X';
+        }
+        System.out.println("Twoje slowo: " + usersText);
+        System.out.println("Zakodowane slowo: " + newWord);
+
+    }
 }

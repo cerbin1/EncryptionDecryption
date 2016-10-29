f1c22a0146d87212c7aeaf24218bfb52cdd8294e
cerbin <bleble@d.com>
2016-10-30 01:15:39 +0200

    FIX

--- src/Application.java
+++ src/Application.java
@@ -10,6 +10,7 @@ public class Application {
     Character[] characters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'y', 'z'};
     int[] charactersNumbers = new int[characters.length];
     String encryptedWord = "";
+    String decryptedWord = "";
 
     public static void main(String[] args) {
         new Application().run();
@@ -18,19 +19,29 @@ public class Application {
     void run() {
 
         for (int i = 0; i < charactersNumbers.length; i++) {
-            charactersNumbers[i] = (int) (Math.round(Math.random() * 127));
+            charactersNumbers[i] = 32 + i;
         }
 
         String usersText = scanner.nextLine();
+
         for (int i = 0; i < usersText.length(); i++) {
             int number = getLettersNumber(usersText.charAt(i));
             encryptedWord += (char) number;
         }
 
+        for (int i = 0; i < usersText.length(); i++) {
+            int number = (int) encryptedWord.charAt(i);
+            System.out.println(number);
+            char character = decryptCharacter(number);
+            decryptedWord += character;
+        }
+
+
         randomNumbers.forEach(System.out::println);
 
         System.out.println("Twoje slowo: " + usersText);
         System.out.println("Zakodowane slowo: " + encryptedWord);
+        System.out.println("Odkodoowane slowo: " + decryptedWord);
     }
 
     int getLettersNumber(char character) {
@@ -40,7 +51,16 @@ public class Application {
             }
         }
         System.out.println("cos sie popsulo");
-            return 0;
+        return 0;
     }
 
+    char decryptCharacter(int number) {
+        for (int i = 0; i < characters.length; i++) {
+            if (number == charactersNumbers[i]) {
+                return characters[i];
+            }
+        }
+        System.out.println("cos sie popsulo");
+        return 'g';
+    }
 }
